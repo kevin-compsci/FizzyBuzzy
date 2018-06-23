fa2ffbedfd59e3d41ef49d8ce5faee269995fac6
Kevin Nguyen <25444103+kevin-compsci@users.noreply.github.com>
2018-06-23 00:41:52 -0500

    Add files via upload

--- Java/JavaFizzyBuzzy.java
+++ Java/JavaFizzyBuzzy.java
@@ -63,13 +63,18 @@ class JavaFizzyBuzzy {
 		//print values up to tgtVal
 		while(i < tgtVal) {
 			//condition to check for fizz, buzz, fizzbuzz respectively
-			if(i % 3 == 0) {
+			if((i % 3 == 0) && (i % 5 == 0)) {
+				System.out.println("FizzBuzz");
+			}
+			else if(i % 3 == 0) {
 				System.out.println("Fizz");
 			}
 			else if(i % 5 == 0) {
 				System.out.println("Buzz");
 			}
-			System.out.println(i);
+			else {
+				System.out.println(i);
+			}
 			i++;
 		}
 	}
